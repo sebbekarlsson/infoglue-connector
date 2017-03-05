02308d0269b298d316be86a4650b6d283d232fed
Sebastian Karlsson <sebastian.karlsson@knowit.se>
2017-03-05 12:41:30 +0100

    correctly load config

--- infogluelocal/utils/config.py
+++ infogluelocal/utils/config.py
@@ -7,4 +7,8 @@ config = {}
 if not os.path.isfile('config.json'):
     print('config.json could not be found.')
 else:
-    config = json.loads('config.json')
+    with open('config.json') as config_file:
+        config_text = config_file.read()
+    config_file.close()
+
+    config = json.loads(config_text)
